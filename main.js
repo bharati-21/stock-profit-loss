ba8ab8b9baae33fba79cdd70a2d1b358a84e6352
Bharati Subramanian <bharatisharada@gmail.com>
2021-08-06 22:39:49 +0530

    feat: change theme

--- main.js
+++ main.js
@@ -2,27 +2,80 @@
 const purchasePriceIp = document.querySelector('.purchase-price');
 const stockQuantityIp = document.querySelector('.stock-quantity');
 const currentPriceIp = document.querySelector('.current-price');
+const main = document.querySelector('.main');
 
-currentPriceIp.value = 110;
+let stockPrice = 110;
+currentPriceIp.value = stockPrice;
+
 
 const btnCheck = document.querySelector('.btn-check');
 const message = document.querySelector('.message');
 
+
 btnCheck.addEventListener('click', checkProfitLoss);
 
 function checkProfitLoss(e) {
-    if(!properValues()) {
-        message.className = "message error";
-        message.innerText = 'Enter proper values';
+    let stockQuantity = stockQuantityIp.value;
+    let purchasePrice = purchasePriceIp.value;
+
+    if(properValues(stockQuantity, purchasePrice)) {
+        stockQuantity = Number(stockQuantity);
+        purchasePrice = Number(purchasePrice);
+
+        const priceDifference = (Math.abs(purchasePrice - stockPrice)).toFixed(2);
+        const profitLoss = (priceDifference*stockQuantity).toFixed(2);
+        const profitLossPercentage = ((priceDifference*100) / purchasePrice).toFixed(2);
+
+        if(purchasePrice > stockPrice) {
+            message.className = "message loss";
+            message.innerHTML = `You lost ${profitLossPercentage}%.<br/>Your loss is: ${priceDifference}/ stock. Total loss is: ${profitLoss}`;
+
+            if(profitLossPercentage > 50) {
+                changeTheme('loss-theme');
+            }
+            else {
+                changeTheme('gain-loss-theme');
+            }
+        }
+        else if(purchasePrice < stockPrice) {
+            message.className = "message profit";
+            message.innerHTML = `You gained ${profitLossPercentage}%.<br/>Your profit is: ${priceDifference}/ stock. Total profit is: ${profitLoss}`;
+
+            if(profitLossPercentage > 50) {
+                changeTheme('profit-theme');
+            }
+            else {
+                changeTheme('gain-loss-theme');
+            }
+        }
+        else {
+            changeTheme('gain-loss-theme');
+            message.className = "message no-gain-loss";
+            message.innerHTML = `You gained 0%.<br/>Your profit is: 0/ stock. Total profit is: 0`;
+        }
     }
 }
 
-function properValues() {
-    const stockQuantity = stockQuantityIp.value;
-    const purchasePrice = purchasePriceIp.value;
-
+function properValues(stockQuantity, purchasePrice) {
     if(purchasePrice === "" || stockQuantity === "" ) {
+        message.className = "message error";
+        message.innerText = 'Please enter all the values';
         return false;
     }
+
+    else if(Number.parseInt(stockQuantity) <=0 || Number.parseInt(purchasePrice) <= 0) {
+        message.className = "message error";
+        message.innerText = 'Please enter values greater than 0';
+        return false;
+    }
+
     return true;
+}
+
+function changeTheme(theme) {
+    main.classList.remove('profit-theme');
+    main.classList.remove('gain-loss-theme');
+    main.classList.remove('loss-theme');
+
+    main.classList.add(theme);
 }
